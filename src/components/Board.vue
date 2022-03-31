2b65f89fbbe4ce4be0368fcdeff6a5ecc44e9be3
edvardgospel <edvard.eros@yahoo.com>
2022-03-31 22:13:37 +0200

    Created components and stylings

---
+++ src/components/Board.vue
@@ -0,0 +1,29 @@
+<template>
+ <div class="board">
+  <button
+   class="field"
+   v-for="(field, i) in fields"
+   :key="i"
+   @click="updateField(i)"
+  >
+   {{ field }}
+  </button>
+ </div>
+</template>
+
+<script>
+
+export default {
+ name: "Board",
+ data() {
+  return {
+   fields: ["", "", "", "", "", "", "", "", ""],
+  };
+ },
+ methods: {
+  updateField(index) {
+   this.fields.splice(index, 1, 'X');
+  },
+ },
+};
+</script>
---
+++ src/components/Game.vue
@@ -0,0 +1,47 @@
+<template>
+ <div class="game">
+  <div class="button-group">
+   <button class="button" @click="this.startGame">
+    {{ this.startGameButtonText }}
+   </button>
+   <button class="button" @click="this.joinGame">Join game</button>
+  </div>
+  <Board v-if="gameStarted" />
+ </div>
+</template>
+
+<script>
+import Board from "./Board.vue";
+
+export default {
+ name: "Game",
+ components: {
+  Board,
+ },
+ data() {
+  return {
+   gameStarted: false,
+  };
+ },
+ computed: {
+  startGameButtonText() {
+   return this.gameStarted ? "Restart Game" : "Start Game";
+  },
+ },
+ methods: {
+  startGame() {
+   if (this.gameStarted) {
+    this.restartGame();
+   } else {
+    this.gameStarted = true;
+   }
+  },
+  restartGame() {
+   console.log("restart game");
+  },
+  joinGame() {
+   console.log("join game");
+  },
+ },
+};
+</script>
